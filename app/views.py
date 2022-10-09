51b9e0f08fdb7e7c30e557c6735a4ef9ca5576c5
matheus costa <matheuscosta.tech@gmail.com>
2022-10-09 13:48:01 -0300

    Listing teachers

--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from app.serializers import TeacherSerializer
+from .models import Teacher
 
 class HomeApiView(APIView):
     def get(self, request, format=None):
-        return Response({'name': 'matheus', 'age': 20}, status=200)
+        teachers = Teacher.objects.all()
+        serializer = TeacherSerializer(teachers, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
