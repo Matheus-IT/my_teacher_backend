ee8aa0cf9b908c4b5e52b209b145c2255d270683
matheus costa <matheuscosta.tech@gmail.com>
2022-10-10 21:25:45 -0300

    Serializers for scheduled class

--- app/serializers.py
+++ app/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
-from .models import Teacher
+from .models import ScheduledClass, Teacher
+
 
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teacher
+        fields = '__all__'
+
+
+class RegisterClassSerializer(serializers.Serializer):
+    email = serializers.EmailField(max_length=255)
+    name = serializers.CharField(max_length=100)
+
+
+class ScheduledClassSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ScheduledClass
         fields = '__all__'
