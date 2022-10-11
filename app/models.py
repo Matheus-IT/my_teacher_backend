82155adf7d979daee1fc1835e623a5f21e3f3e2d
matheus costa <matheuscosta.tech@gmail.com>
2022-10-10 21:22:42 -0300

    New model for ScheduledClass

--- app/models.py
+++ app/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
+
 class Teacher(models.Model):
     name = models.CharField(max_length=100)
     price_per_hour = models.DecimalField(max_digits=9, decimal_places=2)
     description = models.TextField()
     photo = models.URLField(max_length=255)
+
+
+class ScheduledClass(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=255)
+    teacher = models.ForeignKey(
+        Teacher,
+        on_delete=models.CASCADE,
+        related_name='scheduled_classes',
+    )
